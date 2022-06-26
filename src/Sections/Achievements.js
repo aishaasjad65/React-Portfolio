840c19179ac9602b7c1b63df5ce5350f21c33eab
Mursal Furqan <mursalfurqan@gmail.com>
2022-06-26 12:05:52 +0500

    Added Images

--- src/Sections/Achievements.js
+++ src/Sections/Achievements.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Images from '../Images.js'
+import constants from '../constants.js'
 
 export default function Achievements() {
     return (
@@ -8,69 +10,19 @@ export default function Achievements() {
                 <h1 className="text-uppercase font-staat font-size-34">Achievements</h1>
             </div>
             <div className="row text-center">
-                <div className="col-sm-2 pb-1">
-                    <img src="./assets/work/16.png" width="100" height="100" alt="Cisco2"
-                        className="img-fluid tilt" />
-                    <h4 className="font-rami py-2">Cybersecurity Essentials</h4>
-                </div>
-                <div className="col-sm-2 pb-1">
-                    <img src="./assets/work/15.png" width="100" height="100" alt="MCT" className="img-fluid tilt" />
-                    <h4 className="font-rami py-2">Microsoft Certified Trainer</h4>
-                </div>
-                <div className="col-sm-2 pb-1">
-                    <img src="./assets/work/13.png" width="100" height="100" alt="MIE MT"
-                        className="img-fluid tilt" />
-                    <h4 className="font-rami py-2">Microsoft Innovative Educator - Master Trainer 2020-2021</h4>
-                </div>
-                <div className="col-sm-2 pb-1">
-                    <img src="./assets/work/14.png" width="100" height="100" alt="MIEE" className="img-fluid tilt" />
-                    <h4 className="font-rami py-2">Microsoft Innovative Educator Expert 2020-2021</h4>
-                </div>
-                <div className="col-sm-2 pb-1">
-                    <img src="./assets/work/12-01.png" width="100" height="100" alt="MLSA"
-                        className="img-fluid tilt" />
-                    <h4 className="font-rami py-2">Beta - Microsoft Learn Student Ambassador</h4>
-                </div>
-                <div className="col-sm-2 pb-1">
-                    <img src="./assets/work/10.png" width="100" height="100" alt="work1" className="img-fluid tilt" />
-                    <h4 className="font-rami py-2">Data Scientist - DataCamp</h4>
-                </div>
-                <div className="col-sm-2 pb-1">
-                    <img src="./assets/work/08.png" width="100" height="100" alt="work1" className="img-fluid tilt" />
-                    <h4 className="font-rami py-2">MTA - Network Fundamentals</h4>
-                </div>
-                <div className="col-sm-2 pb-1">
-                    <img src="./assets/work/01.png" width="100" height="100" alt="work1" className="img-fluid tilt" />
-                    <h4 className="font-rami py-2">Microsoft Student Partner</h4>
-                </div>
-                <div className="col-sm-2 pb-1">
-                    <img src="./assets/work/09.png" width="100" height="100" alt="work1" className="img-fluid tilt" />
-                    <h4 className="font-rami py-2">IBM - Tools for Data Science</h4>
-                </div>
-                <div className="col-sm-2 pb-1 ">
-                    <img src="./assets/work/02.png" width="100" height="100" alt="work1" className="img-fluid tilt" />
-                    <h4 className="font-rami py-2">MTA - Intro. to Programming using Python</h4>
-                </div>
-                <div className="col-sm-2 pb-1">
-                    <img src="./assets/work/03.png" width="100" height="100" alt="work1" className="img-fluid tilt" />
-                    <h4 className="font-rami py-2">Microsoft Innovative Educator Trainer</h4>
-                </div>
-                <div className="col-sm-2 pb-1">
-                    <img src="./assets/work/04.png" width="100" height="100" alt="work1" className="img-fluid tilt" />
-                    <h4 className="font-rami py-2">Microsoft Office Specialist Expert - Word</h4>
-                </div>
-                <div className="col-sm-2 pb-1">
-                    <img src="./assets/work/05.png" width="100" height="100" alt="work1" className="img-fluid tilt" />
-                    <h4 className="font-rami py-2">Microsoft Office Specialist - Excel</h4>
-                </div>
-                <div className="col-sm-2 pb-1">
-                    <img src="./assets/work/06.png" width="100" height="100" alt="work1" className="img-fluid tilt" />
-                    <h4 className="font-rami py-2">Microsoft Office Specialist - Powerpoint</h4>
-                </div>
-                <div className="col-sm-2 pb-1">
-                    <img src="./assets/work/07.png" width="100" height="100" alt="work1" className="img-fluid tilt" />
-                    <h4 className="font-rami py-2">Microsoft Office Specialist - Word</h4>
-                </div>
+                {
+                    constants.Achievements && constants.Achievements.map((item, index) => {
+                        return (
+                            <>
+                                <div className="col-sm-2 pb-1">
+                                    <img src={item.image} width="100" height="100" alt={item.title}
+                                        className="img-fluid tilt" />
+                                    <h4 className="text-uppercase font-os font-size-16 text-muted py-2">{item.title}</h4>
+                                </div>
+                            </>
+                        )
+                    })
+                }
             </div>
         </section>
     )
--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,5 @@
+import Images from './Images.js'
+
 const scrambled_phrases = [
     'I am React Developer',
     'I am a Web Designer',
@@ -15,7 +17,57 @@ const scrambled_phrases = [
     'I am a UI/UX Engg.'
 ]
 
+const Achievements = [
+    {
+        title: 'Cybersecurity Essentials',
+        image: Images.CyberSecurity,
+    }, {
+        title: 'Microsoft Certified Trainer',
+        image: Images.MCT,
+    }, {
+        title: 'Microsoft Innovative Educator - Master Trainer 2020-2021',
+        image: Images.MIEMT,
+    }, {
+        title: 'Microsoft Innovative Educator Expert 2020-2021',
+        image: Images.MIEExpert,
+    }, {
+        title: 'Beta - Microsoft Learn Student Ambassador',
+        image: Images.MLSABeta,
+    }, {
+        title: 'Data Scientist - DataCamp',
+        image: Images.DSDC,
+    }, {
+        title: 'MTA - Network Fundamentals',
+        image: Images.MTANF,
+    }, {
+        title: 'Microsoft Student Partner',
+        image: Images.MSP,
+    },{
+        title: 'IBM - Tools for Data Science',
+        image: Images.IBMDS,
+    }, {
+        title: 'MTA - Introduction to Python',
+        image: Images.MTAPython,
+    }, {
+        title: 'Microsoft Innovative Educator - Trainer',
+        image: Images.MIETrainer,
+    }, {
+        title: 'Microsoft Office Specialist - Word Expert',
+        image: Images.MOSWordExpert,
+    }, {
+        title: 'Microsoft Office Specialist - Excel',
+        image: Images.MOSExcel,
+    }, {
+        title: 'Microsoft Office Specialist - PowerPoint',
+        image: Images.MOSPPT,
+    }, {
+        title: 'Microsoft Office Specialist - Word',
+        image: Images.MOSWord,
+    }
+]
+
 const constants = {
+    Achievements,
     scrambled_phrases: scrambled_phrases,
 }
 
