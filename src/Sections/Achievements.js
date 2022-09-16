b4c861398941299f01216c3f031e494a213a19e5
Mursal Furqan <mursalfurqan@gmail.com>
2022-09-16 13:04:40 +0500

    Added achievements and certifications to carousal

--- src/Sections/Achievements.js
+++ src/Sections/Achievements.js
@@ -1,5 +1,27 @@
 import React from 'react'
-import constants from '../constants.js'
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import constants from '../constants';
+
+const responsive = {
+    superLargeDesktop: {
+        // the naming can be any, depends on you.
+        breakpoint: { max: 4000, min: 3000 },
+        items: 5
+    },
+    desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 3
+    },
+    tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 2
+    },
+    mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1
+    }
+};
 
 export default function Achievements() {
     return (
@@ -9,19 +31,42 @@ export default function Achievements() {
                 <h1 className="text-uppercase font-staat font-size-34">Achievements</h1>
             </div>
             <div className="row text-center">
-                {
-                    constants.Achievements && constants.Achievements.map((item, index) => {
-                        return (
-                            <>
-                                <div className="col-sm-2 pb-1">
-                                    <img src={item.image} width="100" height="100" alt={item.title}
-                                        className="img-fluid tilt" />
-                                    <h4 className="text-uppercase font-os font-size-16 text-muted py-2">{item.title}</h4>
-                                </div>
-                            </>
-                        )
-                    })
-                }
+                {window.innerWidth < 768 ? (
+                    <>
+                        <div className="carousal_education"
+                            swipeable="true"
+                            draggable="true"
+                            ssr="true" // means to render carousel on server-side.
+                            keyboardcontrol="true"
+                            customdransition="all .5"
+                            transitionduration={500}
+                            containerclass="carousel-container"
+                        >
+                            <Carousel responsive={responsive}>
+                                {constants.Achievements.map((Achievements, index) => (
+                                    <div className="col-sm-2 pb-1">
+                                        <img src={Achievements.image} width="200" height="200" alt={Achievements.title}
+                                            className="img-fluid tilt" />
+                                        <h4 className="text-uppercase font-os font-size-16 text-muted py-2">{Achievements.title}</h4>
+                                    </div>
+                                ))}
+                            </Carousel>
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        {
+                            constants.Achievements && constants.Achievements.map((item, index) => {
+                                return (
+                                    <>
+
+                                    </>
+                                )
+                            })
+                        }
+                    </>
+                )}
+
             </div>
         </section>
     )
